ccc3ca5ef7425ee330647e7bb51fbc309a2eb5a1
LonelyDeev <mahdi77yousefi2015@gmail.com>
2026-06-23 00:44:14 +0330

    first commit

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -57,7 +57,7 @@ function update_url()
         return '';
     }
 
-    $url = 'https://ai.webtpro.ir/updates/' . env('SELF_UPDATER_HTTP_PRIVATE_ACCESS_TOKEN');
+    $url = 'https://update.webtpro.ir/get-update/' . env('SELF_UPDATER_HTTP_PRIVATE_ACCESS_TOKEN');
     $headers_url = @get_headers($url);
     if ($headers_url && strpos($headers_url[0], '200')) {
         return $url . '/';
