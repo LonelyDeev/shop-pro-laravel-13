689c55d7c3e5377b37c08294e9775c04e1cd4a2c
LonelyDeev <mahdi77yousefi2015@gmail.com>
2026-07-21 14:53:33 +0330

    first commit

--- app/Services/PackageInstallerService.php
+++ app/Services/PackageInstallerService.php
@@ -611,9 +611,9 @@ class PackageInstallerService
     {
         try {
             $seederPath = base_path("Modules/{$moduleName}/database/seeders");
-Log::info('Teeeest ',$seederPath);
+Log::info('Teeeest ',[$seederPath]);
             if (!is_dir($seederPath)) {
-                Log::info('faild ',$seederPath.' not found'.$moduleName);
+                Log::info('faild ',['dd'=>$seederPath,' not found'=>$moduleName]);
                 Log::info('No seeders directory found', ['module' => $moduleName]);
                 return;
             }
