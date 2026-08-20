0887a69c06960daa9f391cac95895602ed53255d
LonelyDeev <mahdi77yousefi2015@gmail.com>
2026-08-21 00:26:38 +0330

    first commit

--- themes/WeblakShop/src/resources/views/partials/footer.blade.php
+++ themes/WeblakShop/src/resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@
         </a>
     </div>
     <?php
-    $sevices_sliders=App\Models\Slider::where('group', 'sevices_sliders')
+    $sevices_sliders=App\Models\Slider::where('groups', 'sevices_sliders')
         ->where('published', true)
         ->get()
     ?>
